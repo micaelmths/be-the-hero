dfda72a40e1be2475e7baabce69b4d6061adfca4
micaelmths <micael.matheus@live.com>
2020-03-28 19:28:13 -0300

    front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -42,12 +42,12 @@ module.exports = {
         const { id } = request.params
         const ong_id = request.headers.authorization
 
-        const incidents = await connection('incidents')
+        const incident = await connection('incidents')
         .where('id', id)
         .select('ong_id')
         .first()
 
-        if (incidents.ong_id != ong_id) {
+        if (incident.ong_id != ong_id) {
             return response.status(401).json({ error: 'Operação não permitida!' })
         }
 
